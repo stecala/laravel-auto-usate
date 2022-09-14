e771798bdd8b0af7d1c0a5334090e55e6d2b1704
amaljom <amaljom99@gmail.com>
2022-09-14 18:26:45 +0200

    written the route for car controller

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class CarController extends Controller
 {
-    //
+    public function index(){
+        $cars=car::all();
+        return view('welcome', compact('cars'));
+    }
 }
