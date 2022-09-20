302292baad202dbd7f80cf3262ff05251dc91ec2
Vito Farenga <vitofarenga@libero.it>
2022-09-20 17:28:41 +0200

    edit update

--- resources/views/cars/edit.blade.php
+++ resources/views/cars/edit.blade.php
@@ -3,5 +3,15 @@
 @section('title', 'edit')
      
 @section('main-content')
-    @include('cars.form.form')
+<div class="container-lg">
+    <div class="row py-5">
+       <div class="col-8 offset-2">
+          <form action="{{ route('cars.store') }}" method="POST">
+             @csrf
+             @include('cars.form.form')
+             <button type="submit" class="btn btn-primary">Add car</button>
+          </form>
+       </div>
+    </div>
+ </div>
 @endsection
