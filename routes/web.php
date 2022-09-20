c99be58a63bc8f8144d242e7587a4d2cd54c9c38
stecala <ste.cala123@gmail.com>
2022-09-20 16:02:46 +0200

    feat: :sparkles: index show all data from db

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Car;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function(){
-    return view('cars.index');
-});
+Route::get('/', 'CarController@index');
 Route::resource('/cars', 'CarController');
