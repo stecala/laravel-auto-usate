156455ac9a114c774495bd15b6c733b0b36ccf3f
Antonius131 <antomendetta@gmail.com>
2022-09-20 16:56:55 +0200

    creating create form container

--- resources/views/cars/create.blade.php
+++ resources/views/cars/create.blade.php
@@ -1,9 +1,15 @@
 @extends('layouts.main')
 
 @section('main-content')
-   <form action="{{ route('cars.store') }}" method="POST">
-      @csrf
-      @include('cars.form.form')
-      <button type="submit" class="btn btn-primary">Add car</button>
-   </form>
+   <div class="container">
+      <div class="row">
+         <div class="col-8 col-offset-2">
+            <form action="{{ route('cars.store') }}" method="POST">
+               @csrf
+               @include('cars.form.form')
+               <button type="submit" class="btn btn-primary">Add car</button>
+            </form>
+         </div>
+      </div>
+   </div>
 @endsection
