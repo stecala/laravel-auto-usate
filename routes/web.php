3490d2db9ea2ed767534417ed0fd17ade45ee9a8
Vito Farenga <vitofarenga@libero.it>
2022-10-03 15:41:24 +0200

    api car controller

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'CarController@index');
 Route::resource('/cars', 'CarController');
+Route::get('{any?}', function(){
+    return view('guest.home');
+})->where('any', '.*');
