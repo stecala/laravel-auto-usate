bcf2f36c7d591437b109cb6f7156879fd7c1aaa6
stecala <ste.cala123@gmail.com>
2022-10-03 16:40:16 +0200

    feat: :sparkles: Homepage will show the names of each car

--- resources/js/pages/HomePage.vue
+++ resources/js/pages/HomePage.vue
@@ -1,15 +1,35 @@
 <template>
     <main>
-        
+        <div class="card" v-for="car in cars" :key="car.id">
+            {{ car.car_name }}
+        </div>
     </main>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
+    data:function(){
+        return{
+            cars : [],
+        }
+    },
     methods: {
         //! creo funzione apicall
+        callFunction(){
+           axios.get('/api/Cars').then((response)=>{
+                console.log(response.data.results)
+               this.cars=response.data.results.data;
+           }).catch((error)=>{
+               console.log(error)
+           })
+
+        }
 
     },
+    created(){
+        this.callFunction()
+    }
 }
 </script>
 
